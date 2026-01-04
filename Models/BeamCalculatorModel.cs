3232cb52db8d21e98c77c6c89b04d7de6a4c0147
James Bronder <36022278+jbronder@users.noreply.github.com>
2026-01-04 10:57:21 -0800

    feat: add project files (init commit)

---
+++ Models/BeamCalculatorModel.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.ObjectModel;
+using LiveChartsCore.Defaults;
+
+namespace BeamAnalysisApp.Models;
+
+
+public record BeamLoadCase(string ComboName, string ImageSrc, string LoadKind, int BlcId);
+public class DiagramResults
+{
+    public ObservableCollection<ObservablePoint> MomentSeries { get; set; } = new()
+    {
+        new ObservablePoint(0, 0),
+    };
+
+    public ObservableCollection<ObservablePoint> ShearSeries { get; set; } = new()
+    {
+        new ObservablePoint(0,0),
+    };
+
+    public ObservableCollection<ObservablePoint> DeflectionSeries { get; set; } = new()
+    {
+        new ObservablePoint(0,0),
+    };
+}
+
+/// <summary>
+/// Collects the inputs from the View to be consumed by the .ComputeExtremes and .ComputeDiagrams methods.
+/// </summary>
+public class BeamInputs
+{
+    public double BeamLoad { get; set; } = 0;
+    public double BeamLength { get; set; } = 0;
+    public double BeamInertia { get; set; } = 0;
+    public double BeamElasticity { get; set; } = 0;
+
+    public void Deconstruct(out double load, out double length, out double inertia, out double elasticity)
+    {
+        load = BeamLoad;
+        length = BeamLength;
+        inertia = BeamInertia;
+        elasticity = BeamElasticity;
+    }
+}
+
+/// <summary>
+/// Class that collects the max/min results of the beam load case to update the
+/// View from the ViewModel that 
+/// </summary>
+public class MaxBeamResults
+{
+    public double MaxShear { get; set; } = 0;
+    public double MaxPositiveMoment { get; set; } = 0;
+    public double MaxNegativeMoment { get; set; } = 0;
+    public double MaxDeflection { get; set; } = 0;
+
+    public MaxBeamResults() { }
+    public MaxBeamResults(double shear, double positiveMoment, double negativeMoment, double deflection) { }
+}
+
+public class BeamCalculatorModel
+{
+
+    /// <summary>
+    /// Constant that sets the number of points to generate when computing the
+    /// shear, moment, and deflection diagrams.
+    /// </summary>
+    private const int NUM_POINTS = 1000;
+    public MaxBeamResults ComputeExtremes(int blcId, BeamInputs beamInputs)
+    {
+        switch (blcId)
+        {
+            case 1:
+                return ComputeExtremesBLC1(beamInputs);
+            case 23:
+                return ComputeExtremesBLC23(beamInputs);
+            default:
+                Console.WriteLine("Unhandled Beam Load Case!");
+                return new MaxBeamResults(0, 0, 0, 0);
+        }
+    }
+
+    public DiagramResults ComputeDiagrams(int blcId, BeamInputs beamInputs)
+    {
+        switch (blcId)
+        {
+            case 1:
+                return ComputeDiagramsBLC1(beamInputs);
+            case 23:
+                return ComputeDiagramsBLC23(beamInputs);
+            default:
+                Console.WriteLine("Unhandled Diagram Beam Load Case!");
+                return new DiagramResults();
+        }
+    }
+
+
+    private MaxBeamResults ComputeExtremesBLC1(BeamInputs inputs)
+    {
+        var results = new MaxBeamResults();
+        var (load, length, inertia, elasticity) = inputs;
+        results.MaxPositiveMoment = load * length * length / 8;
+        results.MaxNegativeMoment = 0;
+        results.MaxShear = load * length / 2;
+        results.MaxDeflection = (5 * load * Math.Pow((length), 4)) / (384 * elasticity * inertia);
+        return results;
+    }
+
+    private MaxBeamResults ComputeExtremesBLC23(BeamInputs inputs)
+    {
+        var results = new MaxBeamResults();
+        var (load, length, inertia, elasticity) = inputs;
+        results.MaxPositiveMoment = load * length * length / 24;
+        results.MaxNegativeMoment = load * length * length / 12;
+        results.MaxShear = load * length / 2;
+        results.MaxDeflection = (load * Math.Pow((12 * length), 4)) / (384 * elasticity * inertia);
+        return results;
+    }
+
+    private DiagramResults ComputeDiagramsBLC1(BeamInputs inputs)
+    {
+        var results = new DiagramResults();
+        var (load, length, inertia, elasticity) = inputs;
+        var delta = length / NUM_POINTS;
+
+        double bi = 0;
+        var momentSeries = new ObservableCollection<ObservablePoint>();
+        var shearSeries = new ObservableCollection<ObservablePoint>();
+        var deflectionSeries = new ObservableCollection<ObservablePoint>();
+
+        for (int i = 0; i <= NUM_POINTS; i++)
+        {
+            double mi = (load * bi / 2) * (length - bi);
+            double vi = load * ((length / 2) - bi);
+            double di = ((load * bi) / (24 * elasticity * inertia)) * (Math.Pow(length, 3) - (2 * length * Math.Pow(bi, 2)) + Math.Pow(bi, 3));
+
+            momentSeries.Add(new ObservablePoint(bi, mi));
+            shearSeries.Add(new ObservablePoint(bi, vi));
+            deflectionSeries.Add(new ObservablePoint(bi, di));
+            bi += delta;
+        }
+
+        results.MomentSeries = momentSeries;
+        results.ShearSeries = shearSeries;
+        results.DeflectionSeries = deflectionSeries;
+        return results;
+    }
+
+    private DiagramResults ComputeDiagramsBLC23(BeamInputs inputs)
+    {
+        var results = new DiagramResults();
+        var (load, length, inertia, elasticity) = inputs;
+        var delta = length / NUM_POINTS;
+
+        double bi = 0;
+        var momentSeries = new ObservableCollection<ObservablePoint>();
+        var shearSeries = new ObservableCollection<ObservablePoint>();
+        var deflectionSeries = new ObservableCollection<ObservablePoint>();
+
+        for (int i = 0; i <= NUM_POINTS; i++)
+        {
+            double mi = (load / 12) * (6 * length * bi - Math.Pow(length, 2) - (6 * Math.Pow(bi, 2)));
+            double vi = (load) * ((length / 2) - bi);
+            double di = (load * Math.Pow(bi, 2) / (24 * elasticity * inertia)) * (Math.Pow(length - bi, 2));
+
+            momentSeries.Add(new ObservablePoint(bi, mi));
+            shearSeries.Add(new ObservablePoint(bi, vi));
+            deflectionSeries.Add(new ObservablePoint(bi, di)); 
+            bi += delta;
+        }
+
+        results.MomentSeries = momentSeries;
+        results.ShearSeries = shearSeries;
+        results.DeflectionSeries = deflectionSeries;
+        return results;
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using SkiaSharp;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using BeamAnalysisApp.Models;
+using System.Runtime.CompilerServices;
+using System.Text;
+using LiveChartsCore.Defaults;
+using System.Collections.Generic;
+
+
+namespace BeamAnalysisApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private BeamLoadCase? _blc;
+    private Bitmap? _currentImage;
+    private string? _strBeamLoad;
+    private string? _strBeamLength;
+    private string? _strBeamInertia;
+    private string? _strBeamElasticity;
+
+    private BeamInputs _currentBeamInputs;
+    private MaxBeamResults _maxBeamResults;
+
+    private ObservableCollection<ObservablePoint> _momentData;
+    private ObservableCollection<ObservablePoint> _shearData;
+    private ObservableCollection<ObservablePoint> _deflectionData;
+
+    public ObservableCollection<ISeries> MomentSeries { get; set; }
+    public ObservableCollection<ISeries> ShearSeries { get; set; }
+    public ObservableCollection<ISeries> DeflectionSeries { get; set; }
+
+    private BeamCalculatorModel _bcm;
+
+    public MaxBeamResults MaxResults
+    {
+        get => _maxBeamResults;
+        set => SetProperty(ref _maxBeamResults, value);
+    }
+
+    public ObservableCollection<ObservablePoint> MomentData
+    {
+        get => _momentData;
+        set
+        {
+            SetProperty(ref _momentData, value);
+            Console.WriteLine("SetProp called");
+        }
+    }
+    public ObservableCollection<ObservablePoint> ShearData
+    {
+        get => _shearData;
+        set => SetProperty(ref _shearData, value);
+    }
+    public ObservableCollection<ObservablePoint> DeflectionData
+    {
+        get => _deflectionData;
+        set => SetProperty(ref _deflectionData, value);
+    }
+
+    public string? BeamLoadLabel
+    {
+        get
+        {
+            if (SelectedBeamLoadCase?.LoadKind == "UNIFORM")
+            {
+                return "Uniform Load, w (k/ft)";
+            }
+            else if (SelectedBeamLoadCase?.LoadKind == "POINT")
+            {
+                return "Point Load, P (k)";
+            }
+            return null;
+        }
+    }
+    public string? BeamLoad
+    {
+        get => _strBeamLoad;
+        set
+        {
+            SetProperty(ref _strBeamLoad, value);
+            if (double.TryParse(_strBeamLoad, out double load))
+            {
+                _currentBeamInputs.BeamLoad = load;
+            }
+            // TODO: Needs error handling here?
+            OnInputUpdate();
+        }
+    }
+
+    public string? BeamLength
+    {
+        get => _strBeamLength;
+        set
+        {
+            SetProperty(ref _strBeamLength, value);
+            if (double.TryParse(_strBeamLength, out double length))
+            {
+                _currentBeamInputs.BeamLength = length;
+            }
+            // TODO: Needs error handling here?
+            OnInputUpdate();
+        }
+    }
+
+    public string? BeamInertia
+    {
+        get => _strBeamInertia;
+        set
+        {
+            SetProperty(ref _strBeamInertia, value);
+            if (double.TryParse(_strBeamInertia, out double inertia))
+            {
+                _currentBeamInputs.BeamInertia = inertia;
+            }
+            // TODO: Needs error handling here?
+            OnInputUpdate();
+        }
+    }
+
+    public string? BeamElasticity
+    {
+        get => _strBeamElasticity;
+        set
+        {
+            SetProperty(ref _strBeamElasticity, value);
+            if (double.TryParse(_strBeamElasticity, out double elasticity))
+            {
+                _currentBeamInputs.BeamElasticity = elasticity;
+            }
+            // TODO: Needs error handling here?
+            OnInputUpdate();
+        }
+    }
+
+    public Bitmap? CurrentImage
+    {
+        get => _currentImage;
+        set => SetProperty(ref _currentImage, value);
+    }
+
+    public BeamLoadCase? SelectedBeamLoadCase
+    {
+        get => _blc;
+        set
+        {
+            SetProperty(ref _blc, value);
+            Console.WriteLine($"Selected: {_blc}");
+            UpdateImage();
+            OnPropertyChanged(nameof(BeamLoadLabel));
+            OnInputUpdate();
+        }
+    }
+    public ObservableCollection<BeamLoadCase> BeamLoadCases { get; } = new()
+    {
+        // Add Load Cases here.
+        new BeamLoadCase("Simple Beam - Uniform Load", "avares://BeamAnalysisApp/Assets/blc1.png", "UNIFORM", 1),
+        new BeamLoadCase("Beam Fixed At Both Ends - Uniform Load", "avares://BeamAnalysisApp/Assets/blc23.png", "UNIFORM", 23),
+    };
+
+    /// <summary>
+    /// Runs the calculations for the current beam configuration.
+    /// </summary>
+    private void OnInputUpdate()
+    {
+        MaxResults = _bcm.ComputeExtremes(SelectedBeamLoadCase.BlcId, _currentBeamInputs);
+        var diagramData = _bcm.ComputeDiagrams(SelectedBeamLoadCase.BlcId, _currentBeamInputs);
+
+        MomentData.Clear();
+        foreach (var ms in diagramData.MomentSeries)
+        {
+            MomentData.Add(ms);
+        }
+
+        ShearData.Clear();
+        foreach (var ss in diagramData.ShearSeries)
+        {
+            ShearData.Add(ss);
+        }
+
+        DeflectionData.Clear();
+        foreach (var ds in diagramData.DeflectionSeries)
+        {
+            DeflectionData.Add(ds);
+        }
+    }
+
+    private void UpdateImage()
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(SelectedBeamLoadCase?.ImageSrc))
+            {
+                CurrentImage = LoadImage(SelectedBeamLoadCase.ImageSrc);
+            }
+            else
+            {
+                // Retrieve default image of unloaded beam.
+                CurrentImage = LoadImage("avares://BeamAnalysisApp/Assets/blc0.png");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error loading image: {e.Message}");
+            CurrentImage = null;
+        }
+    }
+
+    private Bitmap LoadImage(string resource)
+    {
+        try
+        {
+            var uri = new Uri(resource);
+            return new Bitmap(AssetLoader.Open(uri));
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public MainWindowViewModel()
+    {
+        // Initialize CurrentImage with a simple beam on start-up.
+        CurrentImage = LoadImage("avares://BeamAnalysisApp/Assets/blc0.png");
+
+        // Simple Instantiation to get a model up and running.
+        // TODO: Refactor to use Dependency Injection.
+        _bcm = new BeamCalculatorModel();
+
+        _currentBeamInputs = new BeamInputs();
+        _maxBeamResults = new MaxBeamResults();
+
+        _momentData = new ObservableCollection<ObservablePoint> { new ObservablePoint(0, 0) };
+        _shearData = new ObservableCollection<ObservablePoint> { new ObservablePoint(0, 0) };
+        _deflectionData = new ObservableCollection<ObservablePoint> { new ObservablePoint(0, 0) };
+
+        MomentSeries = new ObservableCollection<ISeries> {
+            new LineSeries<ObservablePoint> {
+                Values = MomentData,
+                Stroke = new SolidColorPaint(SKColors.CornflowerBlue) { StrokeThickness = 2 },
+                Fill = null,
+                GeometryFill = null,
+                GeometryStroke = null,
+                LineSmoothness = 0,
+            }
+        };
+
+        ShearSeries = new ObservableCollection<ISeries> {
+            new LineSeries<ObservablePoint> {
+                Values = ShearData,
+                Stroke = new SolidColorPaint(SKColors.CornflowerBlue) { StrokeThickness = 2 },
+                Fill = null,
+                GeometryFill = null,
+                GeometryStroke = null,
+                LineSmoothness = 0,
+            }
+        };
+
+        DeflectionSeries = new ObservableCollection<ISeries> {
+            new LineSeries<ObservablePoint> {
+                Values = DeflectionData,
+                Stroke = new SolidColorPaint(SKColors.CornflowerBlue) { StrokeThickness = 2 },
+                Fill = null,
+                GeometryFill = null,
+                GeometryStroke = null,
+                LineSmoothness = 0,
+            }
+        };
+    }
+}
